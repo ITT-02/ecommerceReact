d787f3f3753faf32c40d907fa42718515fce9867
ITT-02 <itintayattito@gmail.com>
2026-05-11 20:59:14 -0500

    Se agrega pagina para movimiento,se amplia iconos de accion en componete tabla y tamaño dinamico de columna

---
+++ src/pages/admin/inventory/MovementsPage.jsx
@@ -0,0 +1,14 @@
+import { PlaceholderPage } from '../../../components/common/PlaceholderPage';
+
+export const MovementsPage = () => {
+  return (
+     <PlaceholderPage
+      title="Historial de Movimientos"
+      description="Registro detallado de entradas, salidas y ajustes de inventario."
+    >
+
+
+    </PlaceholderPage>
+        
+  )
+}
